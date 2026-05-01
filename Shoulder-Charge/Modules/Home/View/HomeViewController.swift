122d58c19afac224a19eaa7d19223693e747f9b4
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-05-01 14:39:45 -0700

    feat: implement HomeViewController with collection view and localization support

---
+++ Shoulder-Charge/Modules/Home/View/HomeViewController.swift
@@ -0,0 +1,74 @@
+//
+//  HomeViewController.swift
+//  Shoulder-Charge
+//
+//  Created by siam on 01/05/2026.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    let images = ["soccer", "basket", "tennis", "racquet"]
+    let names  = [L10n.Home.soccer,L10n.Home.basket,L10n.Home.tennis,L10n.Home.racquet]
+    @IBOutlet weak var collection: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collection.delegate = self
+        collection.dataSource = self
+        collection.collectionViewLayout = createLayout()
+        
+        collection.register(
+            UINib(nibName: "HomeCollectionCell", bundle: nil),
+            forCellWithReuseIdentifier: "HomeCollectionCell"
+        )
+    }
+    
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.6))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8)
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionCell", for: indexPath) as! HomeCollectionCell
+     
+        cell.configure(
+            image: UIImage(named: images[indexPath.row]),
+            title: names[indexPath.row]
+        )
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected:")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            cell.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            UIView.animate(withDuration: 0.2) {
+                cell.transform = .identity
+            }
+        }
+    }
+}
