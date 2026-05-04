e38c557f5185d059787064effe93fe745ea5572f
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-05-04 09:56:57 -0700

    feat: implement Favorite module architecture with MVP pattern and localized empty state

---
+++ Shoulder-Charge/Modules/Favorite/Presentation/Router/FavoriteRouterProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  FavoriteRouterProtocol.swift
+//  Shoulder-Charge
+//
+//  Created by siam on 04/05/2026.
+//
+
+protocol FavoriteRouterProtocol {
+    func navigateToLeagueDetails(with league: UnifiedLeagueModel, from view: FavoriteViewProtocol?)
+}
