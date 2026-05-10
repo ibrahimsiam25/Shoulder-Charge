a3116826e5ce515668a53616249b91e5fb1f726a
EslamElnady0 <eslamelnady254@gmail.com>
2026-05-09 17:29:58 -0700

    Removed APi Key from codebase. Using Secrets file now

--- Shoulder-Charge/Core/Helpers/Constants.swift
+++ Shoulder-Charge/Core/Helpers/Constants.swift
@@ -5,9 +5,11 @@
 //  Created by Eslam Elnady on 29/04/2026.
 //
 
+import Foundation
+
 struct Constants{
     static let baseUrl = "https://apiv2.allsportsapi.com/"
-    static let apiKey = "5ffd0a125be1a6ecc320fb6cd7978e9246ef0660806cdf9644e383cb59061a5d"
+    static let apiKey  = Bundle.main.infoDictionary?["API_KEY"] as? String ?? ""
     static let fixtures = "Fixtures"
     static let teams = "Teams"
     static let leagues = "Leagues"
